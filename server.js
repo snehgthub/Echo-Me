87c1e8354237baf4c5a76a722bd61ff0b3ec138e
Sneh Chauhan <srchauhan.ind@gmail.com>
2024-03-29 12:56:08 +0530

    Updated Version

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const path = require('path')
 const dotenv = require('dotenv').config()
 const { MongoClient } = require('mongodb')
 
+const port = process.env.PORT || 3000
 const dbUsername = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
+
 const url = `mongodb+srv://${dbUsername}:${dbPassword}@userprofileapp.y7n0ndp.mongodb.net/?retryWrites=true&w=majority&appName=UserProfileApp`
 const client = new MongoClient(url)
 
@@ -16,18 +18,18 @@ const collectionName = 'employees'
 app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '/dist')))
 
-app.get('/get-profile', async function(req, res){
+app.get('/get-profile', async function (req, res) {
 
     await client.connect()
 
     const db = client.db(dbName)
     const collection = db.collection(collectionName)
 
-    const result = await collection.findOne({id: 1})
+    const result = await collection.findOne({ id: 1 })
     console.log(result)
 
     response = {}
-    
+
     if (result !== null) {
         response = {
             name: result.name,
@@ -39,7 +41,7 @@ app.get('/get-profile', async function(req, res){
     client.close()
 })
 
-app.post('/update-profile', async function(req, res){
+app.post('/update-profile', async function (req, res) {
     const payload = req.body
 
     await client.connect()
@@ -48,19 +50,19 @@ app.post('/update-profile', async function(req, res){
     const collection = db.collection(collectionName)
 
     if (Object.keys(payload).length === 0) {
-        res.status(400).send({error: "Empty payload. Couldn't update user profile."})
+        res.status(400).send({ error: "Empty payload. Couldn't update user profile." })
     }
     else {
         payload['id'] = 1;
         const updatedValues = { $set: payload }
-        await collection.updateOne({id: 1}, updatedValues, {upsert: true})
-        
+        await collection.updateOne({ id: 1 }, updatedValues, { upsert: true })
+
         // console.log(updatedValues)
-        res.status(200).send({info: "User profile data updated successfully."})
+        res.status(200).send({ info: "User profile data updated successfully." })
         client.close()
     }
 })
 
-app.listen(process.env.PORT, function () {
-    console.log('App listening on port 3000')
+app.listen(port, function () {
+    console.log('App listening on port 3000' + port)
 })
